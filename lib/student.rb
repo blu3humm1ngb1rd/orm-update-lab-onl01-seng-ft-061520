62b701b6d51738688148910ddf14a530a3989d57
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-20 03:52:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student
     VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT MAX(ID) AS LastID FROM students"
+    @id = DB[:conn].execute("SELECT MAX(ID) AS LastID FROM students")[0][0]
   end 
   
   def self.create 
