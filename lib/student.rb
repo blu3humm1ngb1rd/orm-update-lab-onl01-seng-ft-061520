89a34397a50c95e8159f4a662817ff18401ad4bf
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-20 03:51:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student
     VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT "
+    @id = DB[:conn].execute("SELECT MAX(ID)"
   end 
   
   def self.create 
